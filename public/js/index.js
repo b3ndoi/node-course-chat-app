b2ea7a2d795b9927a4b0bd917c88a789f586fffb
Luka Bulatovic <luka.bulatovic.w@gmail.com>
2017-01-29 21:47:17 +0100

    Adding chat style

--- public/js/index.js
+++ public/js/index.js
@@ -9,11 +9,20 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-  console.log('New message', message);
-  var li = jQuery('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery("#message-template").html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
 
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
+  // var formattedTime = moment(message.createdAt).format('h:mm a');
+  // var li = jQuery('<li></li>');
+  // li.text(`${message.from} ${formattedTime}: ${message.text}`);
+  //
+  // jQuery('#messages').append(li);
 });
 
 jQuery('#message-form').on('submit', function(e){
